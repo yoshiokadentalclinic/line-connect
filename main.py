cf82b1098c0fd6a57b6961809d40fdd4f7c8751b
yoshiokadentalclinic <yoshiokadentalclinic31@gmail.com>
2025-07-04 21:20:52 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "LINE Bot is running on Render!"
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    print("✅ LINEからリクエストを受信しました")
+    return "OK"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
